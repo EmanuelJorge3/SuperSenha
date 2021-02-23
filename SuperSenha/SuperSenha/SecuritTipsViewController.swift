113c9a235bf6bf28cf86c9f2a96388739ee76197
EmanuelJorge3 <79527810+EmanuelJorge3@users.noreply.github.com>
2021-02-23 17:17:53 +0100

    Add files via upload
    Ele gera senha ao usuario usando algoritmo de criptografia

---
+++ SuperSenha/SuperSenha/SecuritTipsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecuritTipsViewController.swift
+//  SuperSenha
+//
+//  Created by Emanuel on 02/10/19.
+//  Copyright © 2019 3MA. All rights reserved.
+//
+
+import UIKit
+
+class SecuritTipsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+   
+}
